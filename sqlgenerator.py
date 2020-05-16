41556a1bd90adb2b9e4d9f06923cfe0ff84ab8a3
Ahmet Gedemenli <afgedemenli@gmail.com>
2020-05-16 15:26:51 +0300

    Add id to sql

--- sqlgenerator.py
+++ sqlgenerator.py
@@ -6,10 +6,11 @@ def generate_sql(entities, app_name):
     for entity in entities:
         # Create class
         entity_name = entity.get("entity_name")
-        file.write("CREATE TABLE " + entity_name + " (\n")
+        file.write("CREATE TABLE " + entity_name + "s (\n")
+        file.write("\tid bigint")
         columns = entity.get("fields")
         for column in columns:
             column_name = column.get("field_name")
             data_type = column.get("type")
-            file.write(column_name + " " + data_type + ",\n")
-        file.write(");\n")
+            file.write(",\n\t" + column_name + " " + data_type)
+        file.write("\n);\n")
